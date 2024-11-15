9487cdb580d65789e0b939ddd916aff5216c6ab1
SebasThe21 <sebastianarcos2106@gmail.com>
2024-11-15 17:18:23 -0500

    change

--- javaScript.js
+++ javaScript.js
@@ -1,8 +1,14 @@
-//First ¿How do i can make the computer choose an option?
+const btnRock=document.getElementById('rock');
+const btnPaper=document.getElementById('paper');
+const btnScissors=document.getElementById('scissors');
+const buttonContainer=document.querySelector('.buttons')
+const score=document.querySelector('#socre-line')
 
-let numberAl=()=>Math.floor(Math.random()*3);
-
-function computerChoice(){
+let userScore=0;
+let computeScore=0;
+let rounds=0;
+let computerChoice=function(){
+    let numberAl=()=>Math.floor(Math.random()*3);
 let choiceNumber=numberAl();
     let computerChoice;
     if(choiceNumber===0){
@@ -14,7 +20,109 @@ let choiceNumber=numberAl();
     }
 return computerChoice;
 }
-let computerScore=0;
+
+
+
+
+function selectFunction(choice){
+    const existingMessage=document.querySelector('#selectMessage');
+    if(existingMessage){
+        existingMessage.remove();
+    }
+
+    const selectButton=document.createElement('p');
+    selectButton.style.fontSize="20px";
+    selectButton.style.fontWeight="300";
+    selectButton.textContent=`You have selected: ${choice}`;
+    selectButton.id='selectMessage'
+    buttonContainer.appendChild(selectButton);
+}
+
+function playGame(userChoice,computerChoice){
+    let whoWins
+   
+        
+        if(userChoice.toLowerCase()==="scissors" && computerChoice.toLowerCase() ==="paper"){
+            
+            whoWins="User win"
+        
+        }else if(userChoice.toLowerCase() ==="rock" && computerChoice.toLowerCase() ==="scissors"){
+            
+            whoWins="User win"
+        }else if(userChoice.toLocaleLowerCase() ==="paper" && computerChoice.toLowerCase() ==="rock"){
+            
+            whoWins="user win"
+        }else if(userChoice.toLowerCase()===computerChoice.toLowerCase()){
+            
+            whoWins="draw"
+        }else{
+            
+            whoWins="computer win"
+        }
+    
+        return whoWins
+   
+
+
+}
+
+let result=function(whoWins){
+    if (whoWins.toLocaleLowerCase() === "user win") {
+        rounds++;
+        userScore++;
+        score.textContent = "user win"+" User score: "+userScore+" computer score: "+computeScore;
+    } else if (whoWins.toLocaleLowerCase() === "draw") {
+        rounds++;
+        score.textContent = "draw"+" User score: "+userScore+" computer score: "+computeScore;
+    } else {
+        rounds++;
+        computeScore++;
+        score.textContent = "computer wins"+" User score: "+userScore+" computer score: "+computeScore;
+    }
+}
+
+function buttonEvent(playerDesicion){
+    selectFunction(playerDesicion);
+    let whoWins=playGame(playerDesicion,computerChoice());
+    if(rounds ==5){
+        if(computeScore>userScore){
+
+            score.textContent="The computer is the winner "+" Score: "+computeScore
+        }else{
+            score.textContent="The user is the winner "+" Score: "+userScore
+        }
+    }else{
+
+        result(whoWins);
+    }
+}
+
+btnRock.addEventListener('click',function(){
+
+    buttonEvent("rock")
+
+    
+
+
+    
+});
+
+btnPaper.addEventListener('click',function(){
+    buttonEvent("paper")
+})
+
+btnScissors.addEventListener('click',function(){
+    buttonEvent("scissors")
+})
+
+
+
+
+//First ¿How do i can make the computer choose an option?
+
+
+
+/*let computerScore=0;
 let userScore=0;
 for(let i=0;i<5;i++){
     let userChoice=prompt("Insert your choice( Scissors,Rock or paper)")
@@ -38,13 +146,13 @@ for(let i=0;i<5;i++){
     }
 }
 
-if(computerScore>userScore){
+/*if(computerScore>userScore){
     alert("computer WINNNNSSSS score computer: "+computerScore+" UserScore: "+userScore)
 }else if(computerScore ===userScore){
     alert("DRAAAWW, score computer: "+computerScore+" UserScore: "+userScore)
 }else{
     alert("USEEEER WIIIINS, FATALITY, score computer: "+computerScore+" UserScore: "+userScore)
-}
+}*/
 
 
 
